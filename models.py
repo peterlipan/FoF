953e5490a43341e791f23a55d62a4229c2193ad5
johnP-Li <847041172@qq.com>
2024-02-05 16:39:41 +0800

    disable cls token in ViTEmbeddings

--- models.py
+++ models.py
@@ -1,37 +1,39 @@
 import torch
 import torch.nn as nn
-from transformers import Swinv2Model, Swinv2Config, Swinv2PreTrainedModel
+from MyViT import ViTModel, ViTConfig
 
 
-class SwinTransformer(nn.Module):
-    def __init__(self, image_size, num_classes, pretrained="", patch_size=4, window_size=7):
-        super(SwinTransformer, self).__init__()
-        config = Swinv2Config().from_pretrained(pretrained) if pretrained else Swinv2Config()
+class Transformer(nn.Module):
+    def __init__(self, image_size, num_classes, pretrained="", patch_size=4):
+        super(Transformer, self).__init__()
+        config = ViTConfig().from_pretrained(pretrained) if pretrained else ViTConfig()
         config.num_labels = num_classes
         config.image_size = image_size
-        if not pretrained:
-            config.patch_size = patch_size
-            config.window_size = window_size
+        config.patch_size = patch_size
         
         self.config = config
         self.num_classes = num_classes
         
-        self.swin = Swinv2Model(config, add_pooling_layer=True, use_mask_token=True)
+        self.encoder = ViTModel(config, add_pooling_layer=False, use_mask_token=True)
         if pretrained:
-            self.swin = Swinv2Model.from_pretrained(pretrained, config=config, add_pooling_layer=True, use_mask_token=True)
-        self.global_classifier = nn.Linear(self.swin.num_features, config.num_labels)
-        self.local_classifier = nn.Linear(self.swin.num_features, config.num_labels + 10)
+            self.enocer = ViTModel.from_pretrained(pretrained, config=config, add_pooling_layer=False, use_mask_token=True, ignore_mismatched_sizes=True)
+        # replace the official ViT 'pooler' as the real pooling layer
+        self.pooler = nn.AdaptiveAvgPool1d(1)
+        self.global_classifier = nn.Linear(config.hidden_size, num_classes)
+        self.local_classifier = nn.Linear(config.hidden_size, num_classes + 10)
 
         # Initialize weights and apply final processing
         # self.post_init()
 
     def forward(self, x, token_mask=None):
         return_dict = self.config.use_return_dict
-        outputs = self.swin(x, bool_masked_pos=token_mask, return_dict=return_dict)
-        features = outputs[1]
-        global_logits = self.global_classifier(features)
-        local_logits = self.local_classifier(features)
-        return features, global_logits, local_logits
+        outputs = self.encoder(x, bool_masked_pos=token_mask, return_dict=return_dict)
+        sequence_output = outputs.last_hidden_state
+        pooled_output = self.pooler(sequence_output.transpose(1, 2)[:, :, 1:])
+        pooled_output = torch.flatten(pooled_output, 1)
+        global_logits = self.global_classifier(pooled_output)
+        local_logits = self.local_classifier(pooled_output)
+        return pooled_output, global_logits, local_logits
 
 
 class ContrastiveProjectors(nn.Module):
