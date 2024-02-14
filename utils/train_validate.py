a61be44dc5352858b851ee83160e0ab3a9413a9c
johnP-Li <847041172@qq.com>
2024-02-14 12:03:44 +0800

    save the model

--- utils/train_validate.py
+++ utils/train_validate.py
@@ -33,10 +33,11 @@ def train(dataloaders, models, optimizer, scheduler, args, logger):
     local_projectors.train()
     cls_criterion = nn.CrossEntropyLoss()
     start = time.time()
-    
+
     cur_iter = 0
     pos_ratio = 0
-    patch_size = model.module.config.patch_size if isinstance(model, DataParallel) or isinstance(model, DDP) else model.config.patch_size
+    patch_size = model.module.config.patch_size if isinstance(model, DataParallel) or isinstance(model,
+                                                                                                 DDP) else model.config.patch_size
     float_gene_guidance = GeneGuidance(args.batch_size, args.world_size)
     discrete_gene_guidance = MultiHeadContrastiveLoss(args.batch_size, args.world_size, args.temperature, args.dis_gene)
     global_local = RegionContrastiveLoss(args.batch_size, args.temperature, args.world_size)
@@ -46,8 +47,9 @@ def train(dataloaders, models, optimizer, scheduler, args, logger):
             train_loader.sampler.set_epoch(epoch)
         for i, (img1, img2, dis_gene, float_gene, grade) in enumerate(train_loader):
             img1, img2, dis_gene, float_gene, grade = img1.cuda(non_blocking=True), img2.cuda(non_blocking=True), \
-                                                            dis_gene.cuda(non_blocking=True), float_gene.cuda(non_blocking=True), \
-                                                            grade.cuda(non_blocking=True)
+                                                      dis_gene.cuda(non_blocking=True), float_gene.cuda(
+                non_blocking=True), \
+                                                      grade.cuda(non_blocking=True)
 
             # Class activation map
             cam = get_swin_cam(model, img1, grade, smooth=True)
@@ -70,15 +72,17 @@ def train(dataloaders, models, optimizer, scheduler, args, logger):
             neg_cls = args.lambda_neg * cls_criterion(neg_pred, neg_grade)
             cls_loss = global_cls + pos_cls + neg_cls
             # region contrastive loss
-            region_loss = args.lambda_region * global_local(global_region_features, pos_region_features, neg_region_features)
+            region_loss = args.lambda_region * global_local(global_region_features, pos_region_features,
+                                                            neg_region_features)
             # float gene guidance
             global_pos_features = torch.cat((pos_features, features), dim=0)
             global_pos_gene = torch.cat((float_gene, float_gene), dim=0)
             float_gene_loss = args.lambda_float_gene * float_gene_guidance(global_pos_features, global_pos_gene)
             # discrete gene guidance
-            dis_gene_loss = args.lambda_dis_gene * discrete_gene_guidance(global_gene_features, pos_gene_features, neg_gene_features, dis_gene)           
+            dis_gene_loss = args.lambda_dis_gene * discrete_gene_guidance(global_gene_features, pos_gene_features,
+                                                                          neg_gene_features, dis_gene)
 
-            loss = cls_loss + dis_gene_loss + region_loss + float_gene_loss                
+            loss = cls_loss + dis_gene_loss + region_loss + float_gene_loss
 
             if args.rank == 0:
                 train_loss = loss.item()
@@ -106,7 +110,7 @@ def train(dataloaders, models, optimizer, scheduler, args, logger):
             if args.rank == 0:
                 if cur_iter % 1000 == 1 and logger is not None:
                     # pick 3 images from a batch
-                    wandb_imgs = img1.permute(0,2,3,1).detach().cpu().numpy()[:4]
+                    wandb_imgs = img1.permute(0, 2, 3, 1).detach().cpu().numpy()[:4]
                     wandb_imgs = [(item - np.min(item)) / np.ptp(item) for item in wandb_imgs]
                     wandb_cams = cam.detach().cpu().numpy()[:5]
                     # resize the images and cams to 224x224
@@ -114,7 +118,7 @@ def train(dataloaders, models, optimizer, scheduler, args, logger):
                     wandb_cams = [cv2.resize(item, (224, 224)) for item in wandb_cams]
                     img_cam = [show_cam_on_image(img1, cam, use_rgb=True) for img1, cam in zip(wandb_imgs, wandb_cams)]
                     logger.log({'Image with CAM': [wandb.Image(item) for item in img_cam],
-                    'Original Image': [wandb.Image(item) for item in wandb_imgs]})
+                                'Original Image': [wandb.Image(item) for item in wandb_imgs]})
                 if cur_iter % 100 == 1:
                     cur_lr = optimizer.param_groups[0]['lr']
                     test_acc, test_f1, test_auc, test_bac, test_sens, test_spec, test_prec, test_mcc, test_kappa = validate(
@@ -131,12 +135,12 @@ def train(dataloaders, models, optimizer, scheduler, args, logger):
                                              'Kappa': test_kappa},
                                     'train': {'loss': train_loss,
                                               'cls_loss': cls_loss_item,
-                                                'global_cls_loss': global_cls_loss,
-                                                'pos_cls_loss': pos_cls_loss,
-                                                'neg_cls_loss': neg_cls_loss,
+                                              'global_cls_loss': global_cls_loss,
+                                              'pos_cls_loss': pos_cls_loss,
+                                              'neg_cls_loss': neg_cls_loss,
                                               'float_gene_loss': float_gene_loss_item,
-                                                'dis_gene_loss': dis_gene_loss_item,
-                                                'pos_ratio': pos_ratio / 100,
+                                              'dis_gene_loss': dis_gene_loss_item,
+                                              'pos_ratio': pos_ratio / 100,
                                               'region_loss': region_loss_item,
                                               'learning_rate': cur_lr}}, )
                     pos_ratio = 0
@@ -145,24 +149,27 @@ def train(dataloaders, models, optimizer, scheduler, args, logger):
                         epoch, args.epochs, i + 1, len(train_loader), time.time() - start,
                         cur_lr, loss.item()), end='', flush=True)
 
-        # validate the model
-        test_acc, test_f1, test_auc, test_bac, test_sens, test_spec, test_prec, test_mcc, test_kappa = validate(test_loader, model)
-        if logger is not None:
-            logger.log({'test': {'Accuracy': test_acc,
-                                 'F1 score': test_f1,
-                                 'AUC': test_auc,
-                                 'Balanced Accuracy': test_bac,
-                                 'Sensitivity': test_sens,
-                                 'Specificity': test_spec,
-                                 'Precision': test_prec,
-                                 'MCC': test_mcc,
-                                 'Kappa': test_kappa}})
-        print(f"Fold {args.fold}, Test Accuracy: {test_acc}, Test F1: {test_f1}, Test AUC: {test_auc}, "
-              f"Test BAC: {test_bac}, Test Sensitivity: {test_sens}, Test Specificity: {test_spec}, "
-              f"Test Precision: {test_prec}, Test MCC: {test_mcc}, Test Kappa: {test_kappa}")
-        model_path = os.path.join(args.checkpoints, f"fold_{args.fold}_acc_{test_acc}.pth")
-        state_dict = model.module.state_dict() if isinstance(model, DataParallel) or isinstance(model, DDP) else model.state_dict()
-        torch.save(state_dict, model_path)
+        # validate the model for each epoch
+        if args.rank == 0:
+            test_acc, test_f1, test_auc, test_bac, test_sens, test_spec, test_prec, test_mcc, test_kappa = validate(
+                test_loader, model)
+            if logger is not None:
+                logger.log({'test': {'Accuracy': test_acc,
+                                     'F1 score': test_f1,
+                                     'AUC': test_auc,
+                                     'Balanced Accuracy': test_bac,
+                                     'Sensitivity': test_sens,
+                                     'Specificity': test_spec,
+                                     'Precision': test_prec,
+                                     'MCC': test_mcc,
+                                     'Kappa': test_kappa}})
+            print(f"Fold {args.fold}, Test Accuracy: {test_acc}, Test F1: {test_f1}, Test AUC: {test_auc}, "
+                  f"Test BAC: {test_bac}, Test Sensitivity: {test_sens}, Test Specificity: {test_spec}, "
+                  f"Test Precision: {test_prec}, Test MCC: {test_mcc}, Test Kappa: {test_kappa}")
+            model_path = os.path.join(args.checkpoints, f"fold_{args.fold}_acc_{test_acc}.pth")
+            state_dict = model.module.state_dict() if isinstance(model, DataParallel) or isinstance(model,
+                                                                                                    DDP) else model.state_dict()
+            torch.save(state_dict, model_path)
 
 
 def validate(dataloader, model):
